30de35b076714b6676cc1c975066319e34818bc3
Fendy <xingfen.fendy@outlook.com>
2026-03-18 02:30:40 +0800

    update docker files
    fix: update init

--- backend/internal/repository/init.go
+++ backend/internal/repository/init.go
@@ -1,10 +1,12 @@
 package repository
 
 import (
+	"github.com/XingfenD/yoresee_doc/internal/repository/config_repo"
 	"github.com/XingfenD/yoresee_doc/internal/repository/document_repo"
 	"github.com/XingfenD/yoresee_doc/pkg/storage"
 )
 
 func MustInit() {
 	document_repo.Init(storage.KVS)
+	config_repo.Init(storage.KVS)
 }
